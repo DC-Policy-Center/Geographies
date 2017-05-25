f35325cf2e3e36188a82512b82b402d3a03efdd0
MW-DC-Policy-Center <michael@dcpolicycenter.org>
2017-05-25 15:02:18 -0400

    adding in two new scripts and geojson files, starting a library for location finding

---
+++ geographyFinder.py
@@ -0,0 +1,59 @@
+'''
+Dependencies:
+    *Self built methods*
+    -NONE
+    *Libraries*
+    - json
+    - requests
+    - shapely
+    - csv
+This is an early version of mapping addresses to different geographies,
+starting with DC Wards
+
+|Signature-------------------------------------------|
+|Written for DC Policy Center by                     |
+|Michael Watson & Dart Howell; 2017                  |
+|www.DCPolicyCenter.org / DC-Policy-Center.github.io |
+|github:                                             |
+|       - M-Watson & MW-DC-Policy-Center             |
+|       - DART ADD YOUR GITHUB USERNAME              |
+|----------------------------------------------------|
+'''
+import json
+import requests
+from shapely.geometry import shape, Point
+import csv
+
+def getWard(lon,lat,map_from):
+
+    if map_from.lower() == 'local':
+        #If loading geoJson from local file
+        wardGeoJson_filename = '.\\GeoJson\\Ward_2012.geojson'
+        # load GeoJSON file containing sectors
+        with open(wardGeoJson_filename) as f:
+            geo_json_data = json.load(f)
+    elif map_from.lower() == 'api':
+# Call dc open data API for 2012 ward geoJson file
+        geo_json_raw = requests.get('https://opendata.arcgis.com/datasets/0ef47379cbae44e88267c01eaec2ff6e_31.geojson')  #This should be the 2012 ward map from DC Open Gov
+        geo_json_data = geo_json_raw.json()
+        # construct point based on lon/lat returned by geocoder
+    point = Point(lon, lat)
+
+    # check each polygon to see if it contains the point
+    for feature in geo_json_data['features']:
+        polygon = shape(feature['geometry'])
+        if polygon.contains(point):
+            try:
+                ward_value = feature['properties']['WARD_ID']
+            except:
+                ward_value = '!!NOTFOUND!!'
+    return(ward_value)
+
+'''  *****************        APPENDICES   *************************
+                    I.Changes to look into or needed
+1) Use OS to get current working directory and add in the GeoJson folder for any user
+2) Create a file of methods rather than one time use script
+                    II.
+                    III.
+                    IV.
+*****************        END APPENDICES   *************************'''
---
+++ ll_to_ward.py
@@ -0,0 +1,81 @@
+'''
+Dependencies:
+    *Self built methods*
+    -NONE
+    *Libraries*
+    - json
+    - requests
+    - shapely
+    - csv
+This is an early version of mapping addresses to different geographies,
+starting with DC Wards
+
+|Signature-------------------------------------------|
+|Written for DC Policy Center by                     |
+|Michael Watson & Dart Howell; 2017                  |
+|www.DCPolicyCenter.org / DC-Policy-Center.github.io |
+|github:                                             |
+|       - M-Watson & MW-DC-Policy-Center             |
+|       - DART ADD YOUR GITHUB USERNAME              |
+|----------------------------------------------------|
+'''
+import json
+import requests
+from shapely.geometry import shape, Point
+import csv
+import geographyFinder
+
+data = []
+sales_file = '2016_sales_20011.csv'  # Change to your absolute location for reading in sales file
+with open(sales_file) as csvfile:
+    data_reader = csv.reader(csvfile)
+    for row in data_reader:
+        data.append(row)
+data[0].append('city')
+data[0].append('lat')
+data[0].append('lon')
+data[0].append('ward')
+
+
+
+for i in range(len(data)):
+    if i != 0:
+        data[i].append('Washington, DC')
+
+        ##### This is the LAT LONG LOOKUP WOULD GO
+
+
+
+        #####
+        #### FOR TESTING, ALL SHOULD BE IN 6th Ward.
+        data[i].append(38.885288)
+        data[i].append(-76.993688)
+
+# Find the column number that lat/lon values are located
+lat_index = data[0].index('lat')
+lon_index = data[0].index('lon')
+
+for i in range(len(data)):
+    if i != 0:
+        lat =  data[i][lat_index]
+        lon =  data[i][lon_index]
+
+        ward_value = geographyFinder.getWard(lon,lat,'local')
+
+        data[i].append(ward_value)
+
+
+# Write results to an output CSV file
+with open('hidden_output.csv','w',newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for row in data:
+        csvwriter.writerow(row)
+
+'''  *****************        APPENDICES   *************************
+                    I.Changes to look into or needed
+1) Use OS to get current working directory and add in the GeoJson folder for any user
+2) Create a file of methods rather than one time use script
+                    II.
+                    III.
+                    IV.
+*****************        END APPENDICES   *************************'''
